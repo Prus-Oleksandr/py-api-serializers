d7a7f471a88b318e2e4376a9fc2c45f72da1b785
Prus Oleksandr <cheepaxxa2003@gmail.com>
2026-06-02 23:19:07 +0300

    Implement all serializers and views

--- cinema/models.py
+++ cinema/models.py
@@ -27,8 +27,12 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
-    def __str__(self):
+    @property
+    def get_full_name(self):
         return self.first_name + " " + self.last_name
+
+    def __str__(self):
+        return self.get_full_name
 
 
 class Movie(models.Model):
